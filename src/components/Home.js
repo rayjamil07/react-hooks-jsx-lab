b28c59170416047f881620bef828ee5ba0cae145
rayjamil <ryanwekesa019@gmail.com>
2025-01-20 17:18:30 +0300

    Adds multiple files

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 color="firebrick">{name} is a web developer from {city}</h1>
+    </div>
+  )
 }
 
 export default Home;
